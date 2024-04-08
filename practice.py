f6dec5f52460a2e0a8703ae31176c856f6245e2b
Efe Tascioglu <efe.tascioglu@mail.utoronto.ca>
2024-04-08 01:58:09 -0400

    See notes

---
+++ practice.py
@@ -0,0 +1,10 @@
+import note_seq, pretty_midi
+from note_seq import midi_io
+
+if __name__ == '__main__':
+    data_path = 'data/maestro-v3.0.0'
+    test_path = '/2004/MIDI-Unprocessed_SMF_02_R1_2004_01-05_ORIG_MID--AUDIO_02_R1_2004_05_Track05_wav.midi'
+    midi_data = pretty_midi.PrettyMIDI(data_path + test_path)
+    midi_seq = midi_io.midi_to_note_sequence(midi_data)
+
+    print(midi_seq.notes)
