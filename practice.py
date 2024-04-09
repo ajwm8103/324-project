d738de2541effb984fa083cf705592dd22ca116a
Efe Tascioglu <efe.tascioglu@mail.utoronto.ca>
2024-04-09 17:13:12 -0400

    Better embeddings

--- practice.py
+++ practice.py
@@ -14,14 +14,24 @@ if __name__ == '__main__':
 
     notes_array = np.array(midi_seq.notes)
 
-    input_vector = np.zeros((len(notes_array), 3))
+    input_vector = np.zeros((len(notes_array), 5))
 
-    for i in range(len(notes_array)):
-        input_vector[i][0] = notes_array[i].start_time
-        input_vector[i][1] = notes_array[i].end_time
-        ##input_vector[i][0] = notes_array[i].end_time - notes_array[i].start_time
-        ##input_vector[i][1] = False # Is not a pause
-        input_vector[i][2] = notes_array[i].pitch
-        input_vector[i][3] = notes_array[i].velocity
+    input_vector[0][0] = notes_array[0].end_time - notes_array[0].start_time
+    input_vector[0][1] = 0 # Time between notes
+    input_vector[0][2] = 0
+    input_vector[0][3] = notes_array[0].pitch
+    input_vector[0][4] = notes_array[0].velocity
 
+    for i in range(1, len(notes_array)):
+        ##input_vector[i][0] = notes_array[i].start_time
+        ##input_vector[i][1] = notes_array[i].end_time
+        input_vector[i][0] = notes_array[i].end_time - notes_array[i].start_time
+        input_vector[i][1] = notes_array[i].start_time - notes_array[i-1].end_time # Time between notes
+        input_vector[i][2] = notes_array[i].start_time - notes_array[i-1].start_time # Time since last note started
+        input_vector[i][3] = notes_array[i].pitch
+        input_vector[i][4] = notes_array[i].velocity
+
+
+    # Only 3 Decimals:
+    np.set_printoptions(formatter={'float': lambda x: "{0:0.3f}".format(x)})
     print(input_vector)
