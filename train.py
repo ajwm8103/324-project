d276d1ff8e109ee1b47686f30790c6b9d0562684
Efe Tascioglu <efe.tascioglu@mail.utoronto.ca>
2024-04-12 16:08:44 -0400

    Moved files to folders. Modified parameters for proper batching

--- train.py
+++ train.py
@@ -13,6 +13,22 @@ from note_seq import midi_io
 from embedding import load_embedding_from_pickle
 import numpy as np
 
+def fetch_arguments():
+    parser = argparse.ArgumentParser(description='Transformer Training Script')
+    parser.add_argument("-v", "--verbose", action="store_true", help="increase output verbosity")
+    parser.add_argument("-t", "--tiny", action="store_true", help="Make dataset tiny")
+    parser.add_argument('--epochs', type=int, default=1, help='Number of epochs')
+    parser.add_argument('--seq_len', type=int, default=64, help='Sequence length')
+    parser.add_argument('--batch_size', type=int, default=128, help='Batch size')
+    parser.add_argument('--nhead', type=int, default=5, help='Number of heads in the transformer model')
+    parser.add_argument('--num_encoder_layers', type=int, default=12, help='Number of encoder layers in the transformer model') # TODO: Check this
+    parser.add_argument('--lr', type=float, default=0.001, help='Learning rate')
+    parser.add_argument('--data', type=str, default='data', help='Name of data')
+
+    args = parser.parse_args()
+
+    args.log = print if args.verbose else lambda x: None
+    return args
 
 class MidiDataset(Dataset):
     # In order to capture the elements of music I am combining all tracks together. This means that the Transformer model will loose the "uniqueness" of any individual song.
@@ -22,7 +38,6 @@ class MidiDataset(Dataset):
         self.pad_value = pad_value
         self.padded_data = np.array([np.pad(x, ((0, self.max_len - len(x)), (0, 0)), 'constant', constant_values=self.pad_value) for x in data])
         self.padded_data = self.padded_data.reshape(-1, data[0].shape[1])  # Flatten the data
-
 
     def __len__(self):
         return len(self.padded_data) - self.seq_len
@@ -58,7 +73,7 @@ def main():
             optimizer.step()
             print(loss.item())
 
-    # Save our model:
+    # H2: Save our model:
     torch.save(model.state_dict(), 'transformer_midi_model.pth')
 
     model.eval()
@@ -74,23 +89,6 @@ def main():
 
     # Time to try training with a transformer model
 
-
-def fetch_arguments():
-    parser = argparse.ArgumentParser(description='Transformer Training Script')
-    parser.add_argument("-v", "--verbose", action="store_true", help="increase output verbosity")
-    parser.add_argument("-t", "--tiny", action="store_true", help="Make dataset tiny")
-    parser.add_argument('--epochs', type=int, default=1, help='Number of epochs')
-    parser.add_argument('--seq_len', type=int, default=10, help='Sequence length')
-    parser.add_argument('--batch_size', type=int, default=4, help='Batch size')
-    parser.add_argument('--nhead', type=int, default=5, help='Number of heads in the transformer model')
-    parser.add_argument('--num_encoder_layers', type=int, default=12, help='Number of encoder layers in the transformer model')
-    parser.add_argument('--lr', type=float, default=0.001, help='Learning rate')
-    parser.add_argument('--data', type=str, default='data', help='Name of data')
-
-    args = parser.parse_args()
-
-    args.log = print if args.verbose else lambda x: None
-    return args
 
 if __name__ == '__main__':
     main()
