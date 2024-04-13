5ce42399b93e27c763d7d641fa1c9048b8bdbfdc
Andrew Magnuson <andrew.magnuson@mail.utoronto.ca>
2024-04-12 20:26:18 -0400

    Work for tokens

--- train.py
+++ train.py
@@ -4,14 +4,21 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 from torch.utils.data import DataLoader, Dataset
+from torch.nn.utils.rnn import pad_sequence
 import torch.nn.functional as F
+import time
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
 
 from tqdm import tqdm
 
 import note_seq, pretty_midi
 from note_seq import midi_io
 from embedding import load_embedding_from_pickle
+from transformer import TransformerModel
 import numpy as np
+
+device = torch.device('cuda:1' if torch.cuda.is_available() else 'cpu')
 
 def fetch_arguments():
     parser = argparse.ArgumentParser(description='Transformer Training Script')
@@ -21,7 +28,7 @@ def fetch_arguments():
     parser.add_argument('--seq_len', type=int, default=64, help='Sequence length')
     parser.add_argument('--stride_length', type=int, default=64, help='Stride length')
     parser.add_argument('--batch_size', type=int, default=32, help='Batch size')
-    parser.add_argument('--nhead', type=int, default=8, help='Number of heads in the transformer model')
+    parser.add_argument('--nhead', type=int, default=5, help='Number of heads in the transformer model')
     parser.add_argument('--num_encoder_layers', type=int, default=12, help='Number of encoder layers in the transformer model') # TODO: Check this
     parser.add_argument('--lr', type=float, default=0.001, help='Learning rate')
     parser.add_argument('--data', type=str, default='data', help='Name of data')
@@ -85,21 +92,35 @@ class MidiDatasetToken(Dataset):
         x = self.data[idx].to(dtype=torch.long, device=self.device)  # Ensure data type and device
         y = self.targets[idx].to(dtype=torch.long, device=self.device)
         return x, y
+    
+def collate_fn(batch):
+    inputs, targets = zip(*batch)
+    inputs_padded = pad_sequence(inputs, batch_first=True, padding_value=0)
+    targets_padded = pad_sequence(targets, batch_first=True, padding_value=0)
+    return inputs_padded, targets_padded
 
-def main():
-    args = fetch_arguments()
+def accuracy(model, data):
+    criterion = nn.MSELoss()
+    model.eval()
+    for src, tgt in data:
+        src, tgt = src.to(device), tgt.to(device)
+        output = model(src, tgt)
+        loss = criterion(output, tgt)
+        loss.backward()
+    model.train()
 
+def train_continuous(args):
     # Get embedding
     data_embedding = load_embedding_from_pickle(args)
 
+
+    data_train, data_test = train_test_split(data_embedding, test_size=0.10, random_state=42)
     # Dataset
-    if args.embedding == 'continuous':
-        dataset = MidiDataset(data_embedding, args.seq_len, args, args.stride_length)
-    elif args.embedding == 'token':
-        dataset = MidiDatasetToken(data_embedding, args.seq_len, args, args.stride_length)
+    dataset_train = MidiDataset(data_train, args.seq_len, args, args.stride_length)
+    dataset_test = MidiDataset(data_test, args.seq_len, args, args.stride_length)
 
-    dataloader = DataLoader(dataset, batch_size=args.batch_size, shuffle=True, drop_last=True)
-
+    dataloader_train = DataLoader(dataset_train, batch_size=args.batch_size, shuffle=True, drop_last=True)
+    dataloader_test = DataLoader(dataset_test, batch_size=args.batch_size, shuffle=True, drop_last=True)
     # Other params
     d_model = 5 if args.embedding == 'continuous' else 1
 
@@ -110,9 +131,11 @@ def main():
     optimizer = optim.Adam(model.parameters(), lr=args.lr)
 
     try:
+        iters, train_loss, train_acc, test_acc = [], [], [], []
+        iter_count = 0
         for training_iteration in range(args.train_count // args.batch_size):
             model.train()
-            t = tqdm(dataloader, desc='Loss: N/A')
+            t = tqdm(dataloader_train, desc='Loss: N/A')
             for src, tgt in t:
                 src, tgt = src.to(device), tgt.to(device)
                 optimizer.zero_grad()
@@ -123,6 +146,15 @@ def main():
                 #print(loss.item())
                 t.set_description(f'Loss: {loss.item()}')
                 t.refresh()
+                iter_count += 1
+                if iter_count % 100 == 0:
+                    iters.append(iter_count)
+                    ta = accuracy(model, dataloader_train)
+                    va = accuracy(model, dataloader_test)
+                    train_loss.append(float(loss))
+                    train_acc.append(ta)
+                    test_acc.append(va)
+                    print(iter_count, "Loss:", float(loss), "Train Acc:", ta, "Val Acc:", va)
     except KeyboardInterrupt:
         pass
 
@@ -131,17 +163,110 @@ def main():
 
     model.eval()
     with torch.no_grad():
-        src, tgt = next(iter(dataloader))
+        src, tgt = next(iter(dataloader_train))
         src, tgt = src.to(device), tgt.to(device)
         output = model(src, tgt)  # tgt[:-1] used as target input to predict tgt[1:]
         print("Sample input:", src[0])
         print("Model output:", output[0])
         print("True target:", tgt[1][0])
+    
+    plt.figure()
+    plt.plot(iters[:len(train_loss)], train_loss)
+    plt.title("Loss over iterations")
+    plt.xlabel("Iterations")
+    plt.ylabel("Loss")
+    plt.savefig('figures/train loss.png')
 
+    plt.figure()
+    plt.plot(iters[:len(train_acc)], train_acc)
+    plt.plot(iters[:len(test_acc)], test_acc)
+    plt.title("Accuracy over iterations")
+    plt.xlabel("Iterations")
+    plt.ylabel("Loss")
+    plt.legend(["Train", "Test"])
+    plt.savefig('figures/train test acc.png')
 
 
     # Time to try training with a transformer model
 
+def train_token(args):
+    # Get embedding
+    data_embedding = load_embedding_from_pickle(args)
+
+    # Dataset
+    dataset = MidiDatasetToken(data_embedding, args.seq_len, args, args.stride_length)
+
+    train_data = DataLoader(dataset, batch_size=args.batch_size, shuffle=True, drop_last=True, collate_fn=collate_fn)
+
+    # Other params
+    d_model = 5 if args.embedding == 'continuous' else 1
+
+    device = torch.device('cuda:1' if torch.cuda.is_available() else 'cpu')
+    print("Training on", device)
+    ntokens = 388  # size of vocabulary
+    emsize = 64  # embedding dimension
+    d_hid = 64 # dimension of the feedforward network model in ``nn.TransformerEncoder``
+    nlayers = 2  # number of ``nn.TransformerEncoderLayer`` in ``nn.TransformerEncoder``
+    nhead = 2  # number of heads in ``nn.MultiheadAttention``
+    dropout = 0.2  # dropout probability
+    model = TransformerModel(ntokens, emsize, nhead, d_hid, nlayers, dropout).to(device)
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(model.parameters(), lr=args.lr)
+    scheduler = torch.optim.lr_scheduler.StepLR(optimizer, 1.0, gamma=0.95)
+
+    model.train()  # turn on train mode
+    total_loss = 0.
+    log_interval = 200
+    start_time = time.time()
+
+    try:
+        model.train()
+        t = tqdm(enumerate(train_data), desc='Loss1: N/A', total=args.train_count // args.batch_size)
+        for epoch in range(20):
+            for batch, (data, targets) in t:
+                data, targets = data.to(device), targets.to(device)
+                
+            
+                output = model(data)
+                output_flat = output.view(-1, ntokens)
+                #print('data', data.shape, 'targets', targets.shape, 'output', output.shape, 'flat', output_flat.shape)
+                loss = criterion(output_flat, targets.reshape(-1).contiguous())
+
+                optimizer.zero_grad()
+                loss.backward()
+                torch.nn.utils.clip_grad_norm_(model.parameters(), 0.5)
+                optimizer.step()
+
+                t.set_description(f'Loss: {loss.item()}')
+                t.refresh()
+            torch.save(model.state_dict(), f'models/latest_{args.model}.pth')
+    except KeyboardInterrupt:
+        pass
+
+    # H2: Save our model:
+    torch.save(model.state_dict(), f'models/{args.model}.pth')
+
+    model.eval()
+    with torch.no_grad():
+        src, tgt = next(iter(train_data))
+        src, tgt = src.to(device), tgt.to(device)
+        output = model(src)  # tgt[:-1] used as target input to predict tgt[1:]
+        print("Sample input:", src[0])
+        print("Model output:", output[0])
+        print("True target:", tgt[1][0])
+
+        # Generating an output
+        src = torch.zeros((10,1), dtype=torch.long, device=device)
+        #src = torch.randint(0, 388, (10, 32))  # Example input data [seq_len, batch_size]
+        src_mask = model.generate_square_subsequent_mask(src.size(0)).to(model.device)
+        decoded = model.decode(src)
+
+def main():
+    args = fetch_arguments()
+    if args.embedding == 'token':
+        train_token(args)
+    elif args.embedding == 'continuous':
+        train_continuous(args)
 
 if __name__ == '__main__':
     main()
