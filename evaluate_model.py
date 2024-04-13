0bb62e205c1062387c960d1f849169c3554b637b
Andrew Magnuson <andrew.magnuson@mail.utoronto.ca>
2024-04-12 22:02:14 -0400

    Added tokenized generation

--- evaluate_model.py
+++ evaluate_model.py
@@ -8,13 +8,56 @@ import torch.nn.functional as F
 from train import fetch_arguments
 from embedding import load_embedding_from_pickle, embedding_to_midi
 from train import MidiDataset, MidiDatasetToken, collate_fn
+from pipelines import PerformanceExtractor, extract_performances, Quantizer, TranspositionPipeline, EncoderPipeline
 from transformer import TransformerModel
 import numpy as np
 import random
 import note_seq, pretty_midi
 
-def test_token_model_from_file(args, filename='models/latest_model_1_token.pth'):
+def tokenize_note_sequence(note_sequence):
+    if isinstance(note_sequence, note_seq.NoteSequence):
+        note_sequence = [note_sequence]
+    perf_extractor = PerformanceExtractor(
+        min_events=32,
+        max_events=512,
+        num_velocity_bins=0,
+        note_performance=False,
+    )
+    note_sequence = [perf_extractor.transform(n) for n in note_sequence]
+    note_sequence = [n for ns in note_sequence for n in ns] # Flatten
+
+    #print(len(note_sequence), type(note_sequence[0]))
+
+    encoder_decoder = note_seq.OneHotIndexEventSequenceEncoderDecoder(note_seq.PerformanceOneHotEncoding())
+    #encoder_decoder = note_seq.EventSequenceEncoderDecoder(note_seq.PerformanceOneHotEncoding())
+    encoder_pipeline = EncoderPipeline(encoder_decoder=encoder_decoder, control_signals=None,
+                                        optional_conditioning=False)
+    note_sequence = [encoder_pipeline.transform(n) for n in note_sequence]
+
+    print(note_sequence)
+
+    return note_sequence
+    
+def token_seq_to_midi(token_seq, filename='outputs/token/converted.mid'):
+    encoder_decoder = note_seq.OneHotIndexEventSequenceEncoderDecoder(note_seq.PerformanceOneHotEncoding())
+    for index_out in token_seq:
+        note_performance = note_seq.Performance(steps_per_second=100)
+        for index in index_out:
+            event = encoder_decoder.class_index_to_event(class_index=index.item(), events=[])
+            note_performance.append(event)
+            #print(index, index_out)
+        #encoder_decoder.decode_event()
+
+    generated_sequence = note_performance.to_sequence(
+        max_note_duration=5.0)
+
+    note_seq.sequence_proto_to_midi_file(generated_sequence, filename)
+
+def test_token_model_from_file(emb, args, filename='models/latest_model_1_token.pth', i=0):
     print('Testing!')
+
+    #print(emb)
+
     device = torch.device('cuda:1' if torch.cuda.is_available() else 'cpu')
     ntokens = 388  # size of vocabulary
     emsize = 64  # embedding dimension
@@ -28,26 +71,24 @@ def test_token_model_from_file(args, filename='models/latest_model_1_token.pth')
     model.to(device)
 
     src = torch.zeros((10,1), dtype=torch.long, device=device)
+    src = emb.to(device)
+    # Cast src as long tensor
+    src = src.long()
+    src = src.unsqueeze(0)
     decoded = model.decode(src, max_len=400)
-    print(decoded)
+    #print(decoded)
 
-    # Create decoder, loop over songs and events
-    encoder_decoder = note_seq.OneHotIndexEventSequenceEncoderDecoder(note_seq.PerformanceOneHotEncoding())
-    for index_out in decoded:
-        note_performance = note_seq.Performance(steps_per_second=100)
-        for index in index_out:
-            event = encoder_decoder.class_index_to_event(class_index=index.item(), events=[])
-            note_performance.append(event)
-            #print(index, index_out)
-        #encoder_decoder.decode_event()
-    print(note_performance)
+    print(src.shape, decoded.shape)
+    src_and_decoded = torch.cat((src, decoded), dim=1)
 
-    generated_sequence = note_performance.to_sequence(
-        max_note_duration=5.0)
+    # Just the suffix of the midi
+    token_seq_to_midi(decoded, f'outputs/token/generated_sequence_{i}_suffix.mid')
 
-    print(generated_sequence, type(generated_sequence))
+    # Extended midi
+    token_seq_to_midi(src_and_decoded, f'outputs/token/generated_sequence_{i}_extended.mid')
 
-    note_seq.sequence_proto_to_midi_file(generated_sequence, 'outputs/token/generated_sequence.mid')
+    token_seq_to_midi(src, f'outputs/token/generated_sequence_{i}_src.mid')
+    
 
 def test_model_from_file(dataloader, args, filename="models/latest_model_1.pth"):
     device = torch.device('cuda:1' if torch.cuda.is_available() else 'cpu')
@@ -78,7 +119,9 @@ if __name__ == "__main__":
     data_embedding = load_embedding_from_pickle(args)
 
     if args.embedding == 'token':
-        test_token_model_from_file(args)
+
+        for i in range(10):
+            test_token_model_from_file(data_embedding[i], args, i=i)
     else:
         dataset = MidiDataset(data_embedding, args.seq_len, args, args.stride_length)
         dataloader = DataLoader(dataset, batch_size=args.batch_size, shuffle=True, drop_last=True)
